7231d07a126cc9207e4abe1bbbc802ee80f676c3
psittacine <psittacine714@gmail.com>
2016-06-03 15:10:52 -0500

    Added noscript alternative design.

--- wp-content/themes/accelerate-theme-child/404.php
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -19,26 +19,18 @@ get_header(); ?>
 	<div id="primary" class="site-content">
 <!-- stuff that is constrained in middle boundaries; normal width -->
 
-<div class="matrix-container">
+
 <!-- Matrix Rain effect, jQuery plugin -->
-<div id="matrix">
 
+<div id="matrix" class="matrix-container" style="color:white;"> <!-- style color text white so it is not visible on white bg if noscript -->
   <span>404 Not Found</span>
-
   <span>00110100 00110000 00110100</span>
-
   <span>404 Not Found 404 Not Found 404 Not Found</span>
-
   <span>00110100 00110000 00110100</span>
-
   <span>404 Not Found 404 Not Found</span>
-
   <span>00110100 00110000 00110100</span>
-
   <span>404 Not Found</span>
-
   <span>00110100 00110000 00110100</span>
-
 </div>
 
 <script>
@@ -59,8 +51,26 @@ jQuery( document ).ready( function( $ ) {
 
 } );
 </script>
+
 <!-- END Matrix Rain effect, jQuery plugin -->
-</div>
+
+<!-- Show default map 404 design if noscript -->
+<noscript>
+	<section class="notfound-container">
+		<div class="site-content">
+			<section class="notfound-content">
+				<div class="notfound-content-container">
+					<figure class="fig-notfound"><img src="//localhost/skillcrush/accelerate/wp-content/uploads/2016/06/map404.png" alt="map" /></figure>
+					<div class="txt-notfound">
+						<h3>Whoops, Took a Wrong Turn…</h3>
+						<p>Sorry, this page no longer exists, never existed or has been moved. We feel like complete jerks for totally misleading you.</p>
+						<p>Feel free to take a look around our <a href="blog">blog</a> or some of our featured <a href="case-studies">work</a>.</p>
+					</div>
+				</div>
+	  	</section>
+		</div><!-- .site-content -->
+	</section>
+</noscript>
 
 
 	</div><!-- #primary -->
